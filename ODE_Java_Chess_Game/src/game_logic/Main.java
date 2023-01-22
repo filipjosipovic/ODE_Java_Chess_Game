f88126e93cd5285f1dfe6cc17c776437f3c8a829
filipjosipovic <55580985+filipjosipovic@users.noreply.github.com>
2023-01-22 13:37:53 +0100

    Created Server.java

---
+++ ODE_Java_Chess_Game/src/game_logic/Main.java
@@ -0,0 +1,9 @@
+package game_logic;
+
+public class Main {
+    public static void main(String[] args) {
+        String rook = "♖A1A10";
+        System.out.println(rook.substring(3, 5));
+    }
+
+}
