d6b810f83ecd089ce437bedc0f132bba606b159b
filipjosipovic <55580985+filipjosipovic@users.noreply.github.com>
2023-01-22 13:55:27 +0100

    Added figure logic

---
+++ ODE_Java_Chess_Game/src/figures/Pawn.java
@@ -0,0 +1,25 @@
+package game;
+
+public class Pawn extends Piece {
+    public Pawn(int color, String pos) {
+        super(color, pos);
+    }
+
+    @Override
+    public boolean makeMove(String posFinal) {
+        int finalX = xDecryptor(posFinal);  //getting position in x,y coordinates
+        int finalY = yDecryptor(posFinal);
+
+        if (color == 1){
+            if (finalX == x && finalY <= y-2) {  //pawn logic
+                return true;
+            }
+        }
+
+        if (finalX == x && finalY <= y+2){  //pawn logic
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
